79e6195036f57ef8fc4c64522b74487975af356c
MK <mohamed.khaled33388@gmail.com>
2021-01-02 20:46:01 +0200

    push

--- ArdAPI/settings.py
+++ ArdAPI/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for ArdAPI project.
 
@@ -124,12 +125,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
